27f6960ddf22ef6f465b082e9e5f56d4363b4135
jmaillard <jmaillard@ITEM-S125480>
2024-12-31 15:35:36 +0100

    Change color particles

--- frontend/src/assets/data/app.js
+++ frontend/src/assets/data/app.js
@@ -66,7 +66,7 @@ particlesJS('particles-js',
         "enable": true,
         "distance": 150,
         "color": color,
-        "opacity": 0.2,
+        "opacity": 0.4,
         "width": 1
       },
       "move": {
