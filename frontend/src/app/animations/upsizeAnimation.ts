58f07a225a20f468706b02c0af9d7dd1cb8b9315
julienmld <julienmld.pro@outlook.fr>
2025-01-10 15:33:34 +0100

    Modify bottom banner

--- frontend/src/app/animations/upsizeAnimation.ts
+++ frontend/src/app/animations/upsizeAnimation.ts
@@ -6,7 +6,7 @@ export const upsizeAnimation = trigger('upsize', [
       style({ transform: 'scale(1)', opacity: 1 }),
       animate('150ms ease-out', style({ transform: 'scale(1)', opacity: 1 })),
       stagger('80ms', [
-        animate('200ms ease-out', style({ transform: 'scale(1.2)' })),
+        animate('200ms ease-out', style({ transform: 'scale(1.3)' })),
         animate('300ms ease-out', style({ transform: 'scale(1)' }))
       ])
     ])
